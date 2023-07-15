a0035ad1267bb20cbe8eccce36d0a6d9f3d6c585
Ayah-AQ <ayoya_1998@hotmail.com>
2023-07-15 14:35:00 +0000

    messageQueue

---
+++ messageQueue/manager/manager.js
@@ -0,0 +1,36 @@
+'use strict'
+require('dotenv').config()
+const port = process.env.PORT || 3033;
+const ioClient = require('socket.io-client');
+let host = `http://localhost:${port}/`;
+const managerConnection = ioClient.connect(host);
+const { v4: uuidv4 } = require('uuid');
+const {faker} = require('@faker-js/faker');
+
+  setInterval(() => {
+      const flightId = uuidv4();
+      const destination = faker.location.city();
+      const pilotName = faker.person.firstName();
+      
+      const flight = {
+        event: 'new-flight',
+        time: new Date(),
+        Details: {
+          airLine: 'Royal Jordanian Airlines',
+          destination: destination,
+          pilot: pilotName,
+          flightID: flightId,
+        },
+      };
+
+    console.log('Manager: new flight with ID', flightId, 'has been scheduled');
+  managerConnection.emit('new-flight', flight);
+  }, 10000);
+
+  // managerConnection.on('added Successfully', () => {
+  //   console.log('Thank you for adding')
+  // })
+
+  managerConnection.on('arrived', (flight) => {
+    console.log('Manager: we’re greatly thankful for the amazing flight,', flight.Details.pilot);
+  });
